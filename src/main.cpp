72b22136289d0cc2ce9c845a806221414a121959
Andreas Junemann <ajunemann@hs-koblenz.de>
2020-03-04 19:32:07 +0100

    blynk example
    Signed-off-by: Andreas Junemann <ajunemann@hs-koblenz.de>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include <Arduino.h>
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
-void setup() {
-  // put your setup code here, to run once:
+char auth[] = "Ryi38s3QEXb6hJT8Ek4GT4Bfgkc22erl";
+char ssid[] = "iPhone von Andi";//"TP-Link_45C6";
+char pass[] = "12345678";//"98273171";
+
+BLYNK_WRITE(V5) // V5 is the number of Virtual Pin  
+{
+  int pinValue = param.asInt();
+  Serial.println(pinValue);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
 }
